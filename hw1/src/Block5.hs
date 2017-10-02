4e98d7675208be280d8eae531b83b3eb308384fd
swanwarp <demonican.1996@gmail.com>
2017-10-02 16:05:46 +0400

    hw1 updated

--- hw1/src/Block5.hs
+++ hw1/src/Block5.hs
@@ -7,6 +7,8 @@ module Block5
 import           Data.Maybe (fromMaybe)
 import           Data.Monoid (mempty, mappend)
 import           Data.Either (lefts, rights)
+import           Data.Semigroup
+
 
 maybeConcat :: [Maybe [a]] -> [a]
 maybeConcat [] = []
@@ -18,3 +20,20 @@ monoidConcat x = mappend (head x) (monoidConcat (tail x))
 
 eitherConcat :: (Monoid a, Monoid b) => [Either a b] -> (a, b)
 eitherConcat x = (monoidConcat (lefts x), monoidConcat (rights x))
+
+data NonEmpty a = a :| [a]
+
+instance Semigroup (NonEmpty a)
+  where
+    (x :| xs) <> ~(y :| ys) = x :| (xs ++ y : ys)
+    
+newtype Identity a = Identity {runIdentity :: a}
+
+instance Semigroup a => Semigroup (Identity a)
+  where
+    (<>) (Identity x) (Identity y) = Identity (x <> y)
+
+instance Monoid a => Monoid (Identity a)
+  where
+    mempty = Identity mempty
+    mappend (Identity x) (Identity y) = Identity (mappend x y) 
